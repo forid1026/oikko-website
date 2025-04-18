0dfe5bd7d03d2267dfb4f7c71267887acc8b81a3
nitweb <faridnitbd.com>
2025-04-18 09:47:04 +0600

    navigator added

--- resources/views/livewire/layout/main.blade.php
+++ resources/views/livewire/layout/main.blade.php
@@ -1,4 +1,4 @@
-<header>
+{{-- <header>
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="" wire:click.prevent="setPage('home')" class="flex items-center">
@@ -37,7 +37,7 @@
                             aria-current="page" wire:click.prevent="setPage('home')">Home</a>
                     </li>
                     <li>
-                        <a wire:click.prevent="setPage('about')"  href="#"
+                        <a href="/about" wire:navigate  href="#"
                             class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">About</a>
                     </li>
                     <li>
@@ -62,4 +62,4 @@
     @elseif ($currentPage === 'contact')
         @livewire('pages.contact')
     @endif
-</header>
+</header> --}}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use App\Livewire\Layout\Main;
+use App\Livewire\Pages\About;
+use App\Livewire\Pages\Contact;
+use App\Livewire\Pages\Home;
+use App\Livewire\Pages\Projects;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', Main::class);
+Route::get('/', Home::class);
+Route::get('/about', About::class);
+Route::get('/projects', Projects::class);
+Route::get('/contact', Contact::class);
